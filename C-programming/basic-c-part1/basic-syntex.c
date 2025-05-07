6cbf52141bd3d4ec92d04fbbd98c32abf4004a82
tanvirahammadjoy <tanvirahammadjoy@gmail.com>
2025-05-07 20:19:25 +0800

    Remove hello-C.c and add initial C and JavaScript syntax files

---
+++ C-programming/basic-c-part1/basic-syntex.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+// to compile this code use command "gcc basic-syntex.c -o basic-syntex"
+// to run this code use command "./basic-syntex"
+// main function is the starting point of the program
+// the main is called when the program is executed by the operating system or when the program is started automatically
+// it is the first function that is called when the program is executed
+// and it is the last function that is called before the program exits
+// the return value of the main function is the exit status of the program
+// it can create only one time in a program application
