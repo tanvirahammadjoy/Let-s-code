134561191bfe847a07a732bba30705d10804de34
tanvirahammadjoy <tanvirahammadjoy@gmail.com>
2025-07-23 23:33:02 +0800

    Add routing and page components: Home, About, and Contact; update styles and dependencies

--- js-javascript/reactproject1/frontend/my-react-app/src/App.jsx
+++ js-javascript/reactproject1/frontend/my-react-app/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+import Home from "./pages/home.jsx";
+import About from "./pages/about.jsx";
+import Contact from "./pages/contact.jsx";
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router className="Router">
+
+      <nav className="Nav">
+        <ul>
+          <li><NavLink to="/" className="Home"><a href="/">Home</a></NavLink></li>
+          <li><NavLink to="/about" className="About"><a href="/about">About</a></NavLink></li>
+          <li><NavLink to="/contact" className="Contact"><a href="/contact">Contact</a></NavLink></li>
+        </ul>
+      </nav>
+
+      <Routes className="Routes">
+        <Route className="Home" path="/" element={<Home />} />
+        <Route className="About" path="/about" element={<About />} />
+        <Route className="Contact" path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ js-javascript/reactproject1/frontend/my-react-app/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { useState } from 'react';
+import './home.css';
+
+function Home() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    );
+}
+
+export default Home;
